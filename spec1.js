fe3113a314f5a086a84f387f39af736071cf7b9f
d0riangrey <utyfdfrthby08@gmail.com>
2019-09-15 13:15:47 +0300

    1

---
+++ spec1.js
@@ -0,0 +1,29 @@
+describe('Protractor  baby steps', function () {
+
+	it('Open Angular js website', function () {
+
+
+		//write your raw protractor code
+
+        browser.get('https://angularjs.org');
+
+		
+
+
+	});
+
+	it('close browser', function () {
+
+		//code to close browser
+
+
+	});
+
+
+
+
+
+});
+
+		// first parameter - Test suite name
+		// Second parameter - function (function will have all tests (it blocks))
